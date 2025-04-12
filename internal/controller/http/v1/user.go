2b543d3a39ef395fc0838b7db80cce50325a24d3
irumako <irumakoTS@gmail.com>
2025-04-12 18:46:49 +0300

    feat: implement dummyLogin handler

--- internal/controller/http/v1/user.go
+++ internal/controller/http/v1/user.go
@@ -3,17 +3,20 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	generated "pvz-service/internal/controller/http/dto"
 	"pvz-service/internal/usecase"
+	"pvz-service/pkg/jwt"
 	"pvz-service/pkg/logger"
 )
 
 type userRoutes struct {
-	u usecase.User
-	l logger.Interface
+	u   usecase.User
+	l   logger.Interface
+	jwt *jwt.Jwt
 }
 
-func NewUserRoutes(apiV1 *gin.RouterGroup, u usecase.User, l logger.Interface) {
-	r := &userRoutes{u, l}
+func NewUserRoutes(apiV1 *gin.RouterGroup, u usecase.User, l logger.Interface, jwt *jwt.Jwt) {
+	r := &userRoutes{u, l, jwt}
 
 	routes := apiV1.Group("/")
 	{
@@ -24,7 +27,26 @@ func NewUserRoutes(apiV1 *gin.RouterGroup, u usecase.User, l logger.Interface) {
 }
 
 func (r *userRoutes) getDummyLogin(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{"msg": "success"})
+	var req generated.PostDummyLoginJSONRequestBody
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, generated.Error{Message: err.Error()})
+		return
+	}
+
+	if req.Role != generated.PostDummyLoginJSONBodyRoleEmployee && req.Role != generated.PostDummyLoginJSONBodyRoleModerator {
+		ctx.JSON(http.StatusBadRequest, generated.Error{Message: "Неверный запрос"})
+		return
+	}
+
+	var token generated.Token
+
+	token, err := r.jwt.GenerateToken("dummy-user-id", "dummy@example.com", string(req.Role))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, generated.Error{Message: "Ошибка сервера"})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, token)
 }
 
 func (r *userRoutes) register(ctx *gin.Context) {
