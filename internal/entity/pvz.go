bb4ff9c9ba5e4d914eec110cd9fb42a830e40eca
irumako <irumakoTS@gmail.com>
2025-04-12 18:43:08 +0300

    fix: change pvz entity name

--- internal/entity/pvz.go
+++ internal/entity/pvz.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-type PVZ struct {
+type Pvz struct {
 	ID               string    `json:"id" db:"id"`
 	RegistrationDate time.Time `json:"registration_date" db:"registration_date"`
 	City             string    `json:"city" db:"city"`
